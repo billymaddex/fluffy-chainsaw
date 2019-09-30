4ee31a732a5ab3cee499db520a1f4c0d70b2751f
billymaddex <billy@moonlightrambler.net>
2019-09-30 00:09:49 -0700

    First Draft, code still needs comments
    Signed-off-by: billymaddex <billy@moonlightrambler.net>

---
+++ DesignAssignments/DA2A/T2/main.c
@@ -0,0 +1,49 @@
+/*
+ * DA2AT2.c
+ *
+ * Created: 9/29/2019 7:42:05 PM
+ * Author : Billy
+ */ 
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+	DDRB = 0x00;
+	DDRC = 0x00;
+	DDRD = 0x00;
+	PORTB = 0xFF;
+	PORTC = 0xFF;
+	PORTD = 0xFF;
+    DDRB |= (1 << 3);
+	DDRB |= (1 << 2);
+	DDRC &= ~(1 << 3);
+	//PORTC &= ~(1 << 3);
+	
+    while (1) 
+    {
+		PORTB &= ~(1 << 3);
+		_delay_ms(2500);
+		PORTB |= (1 << 3);
+		_delay_ms(3750);
+		
+		if (~PINC & (1 << 3))
+		{
+			PORTB &= ~(1 << 2);
+			_delay_ms(13330);
+			PORTB |= (1 << 2);
+			//switch_event();
+		}
+    }
+}
+
+int switch_event(void)
+{
+	PORTB &= ~(1 << 2);
+	_delay_ms(13330);
+	PORTB |= (1 << 2);
+	
+	return 0;
+}
