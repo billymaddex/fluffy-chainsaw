387a85da6f2f810a60c33ea24dc7a4f99aba8562
billymaddex <billy@moonlightrambler.net>
2019-09-30 10:03:33 -0700

    Added all comments and removed extra function/call
    Signed-off-by: billymaddex <billy@moonlightrambler.net>

--- DesignAssignments/DA2A/T1/main.asm
+++ DesignAssignments/DA2A/T1/main.asm
@@ -6,43 +6,58 @@
 ;
 
 
-; Set GPIO ports
+	;; Set GPIO ports
+	;; Set DDR B,C,D to known initial value
 	LDI		R25, 0x00
 	OUT		DDRB, R25
 	OUT		DDRC, R25
 	OUT		DDRD, R25
+	;; set PORT B,C,D to know initial value
 	LDI		R25, 0xFF
 	OUT		PORTB, R25
 	OUT		PORTC, R25
 	OUT		PORTD, R25
+	;; set PORTB 2 and 3 to output
 	SBI		DDRB, 3
 	SBI		DDRB, 2
+	;; set PINC 3 to input
 	CBI		DDRC, 3
 
 MAIN_LOOP:
+	;; LED pulse
+	;; turn the light on
 	CBI		PORTB, 3
+	;; call delay at 250ms
 	LDI		R17, HIGH(250)
 	LDI		R16, LOW(250)
-	CALL	DELAY
+	CALL		DELAY
+	;; turn the light off
 	SBI		PORTB, 3
+	;; call delay at 375ms
 	LDI		R17, HIGH(375)
 	LDI		R16, LOW(375)
-	CALL	DELAY
+	CALL		DELAY
 
-	SBIS	PINC, 3
-	CALL	SWITCH_EVENT
+	;; if switch is not pressed, repeat the loop
+	SBIC		PINC, 3
+	RJMP		MAIN_LOOP
 
-	RJMP	MAIN_LOOP
-
-SWITCH_EVENT:
+	;; otherwise, flash the other light
+	;; turn the light on
 	CBI		PORTB, 2
+	;; call delay at 1333ms
 	LDI		R17, HIGH(1333)
 	LDI		R16, LOW(1333)
-	CALL	DELAY
-	SBI		PORTB, 2
+	CALL		DELAY
+	;; turn the light off
+	SBI		PORTB, 2	
 
-	RET
+	;; repeat the loop
+	RJMP		MAIN_LOOP
 
+	;; simple delay function
+	;; takes input as 16-bit int in R17:R16
+	;; delay is supposed to be in ms
 DELAY:
 	LDI		R20, 0
 	MOV		R22, R17
@@ -54,13 +69,13 @@ L2:
 	NOP
 L1:
 	DEC		R24
-	BRNE	L1
+	BRNE		L1
 	DEC		R23
-	BRNE	L2
-	SUBI	R21, 1
-	SBCI	R22, 0
-	BRNE	L3
+	BRNE		L2
+	SUBI		R21, 1
+	SBCI		R22, 0
+	BRNE		L3
 	CP		R21, R20
-	BRNE	L3
+	BRNE		L3
 
 	RET
--- DesignAssignments/DA2A/T2/main.c
+++ DesignAssignments/DA2A/T2/main.c
@@ -11,28 +11,41 @@
 
 int main(void)
 {
-	DDRB = 0x00;
-	DDRC = 0x00;
-	DDRD = 0x00;
-	PORTB = 0xFF;
-	PORTC = 0xFF;
-	PORTD = 0xFF;
-	DDRB |= (1 << 3);
-	DDRB |= (1 << 2);
-	DDRC &= ~(1 << 3);
-	
-    while (1) 
+  // Set DDR B,C,D to known initial value
+  DDRB = 0x00;
+  DDRC = 0x00;
+  DDRD = 0x00;
+  // set PORT B,C,D to know initial value
+  PORTB = 0xFF;
+  PORTC = 0xFF;
+  PORTD = 0xFF;
+  // set PORTB 2 and 3 to output
+  DDRB |= (1 << 3);
+  DDRB |= (1 << 2);
+  // set PINC 3 to input
+  DDRC &= ~(1 << 3);
+
+  // LED pulse
+  while (1) 
     {
-		PORTB &= ~(1 << 3);
-		_delay_ms(2500);
-		PORTB |= (1 << 3);
-		_delay_ms(3750);
-		
-		if (~PINC & (1 << 3))
-		{
-			PORTB &= ~(1 << 2);
-			_delay_ms(13330);
-			PORTB |= (1 << 2);
-		}
+      // turn the light on
+      PORTB &= ~(1 << 3);
+      // call delay
+      _delay_ms(2500);
+      // turn the light off
+      PORTB |= (1 << 3);
+      // call delay
+      _delay_ms(3750);
+
+      // if swith is pressed, flash the other light
+      if (~PINC & (1 << 3))
+	{
+	  // turn the light on
+	  PORTB &= ~(1 << 2);
+	  // call delay
+	  _delay_ms(13330);
+	  // turn the light off
+	  PORTB |= (1 << 2);
+	}
     }
 }
